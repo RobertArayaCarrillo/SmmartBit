4e01847b8e1896741cfbc7f3d7d33d2aad275104
RobertAraya <92409762+RobertArayaCarrillo@users.noreply.github.com>
2024-04-25 01:46:59 -0600

    Add files via upload

---
+++ Security/Autenticacion.cs
@@ -0,0 +1,27 @@
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
+
+namespace Security
+{
+    public class Autenticacion
+    {
+
+        public string ValidacionUsuario(Usuario usuario)
+        {
+            if (usuario.Usuario == "Jaydeep" && usuario.Password == "Pass@777")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSetting["JWT:Secret"]));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(issuer: ConfigurationManager.AppSetting["JWT:ValidIssuer"], audience: ConfigurationManager.AppSetting["JWT:ValidAudience"], claims: new List<Claim>(), expires: DateTime.Now.AddMinutes(6), signingCredentials: signinCredentials);
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                return tokenString;
+            }
+            return null;
+        }
+    }
+}
